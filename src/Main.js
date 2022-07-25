d9eb130d34122919914b2f0397871dfd42a5bd06
= <vapradeep817@gmail.com>
2022-07-25 14:19:32 -0500

    Add dynamic loading of any number of pictures

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import HornedBeast from './HornedBeast.js';
+import data from './data/data.json';
 
 class Main extends React.Component {
     render() {
+
+        let fragment = data.map((picture) => {
+            return <HornedBeast key={picture._id} title={picture.title} imageUrl={picture.image_url} description={picture.description}/>
+        });
+
         return (
             <main>
-                <HornedBeast message="Hello! Hi! Howdy!"/>
+                {fragment}
             </main>
         )
     }
