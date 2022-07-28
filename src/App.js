ae0973ff519f35b65ed9f798be71b9d45f5ca7cc
= <vapradeep817@gmail.com>
2022-07-28 14:46:26 -0500

    Add filter by horns option

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React from 'react';
-import Header from './Header.js';
 import Main from './Main.js';
 
 class App extends React.Component {
@@ -8,7 +7,6 @@ class App extends React.Component {
     render() {
         return (
             <div className='App'>
-                <Header/>
                 <Main/>
                 <footer>Varun Pradeep 2022</footer>
             </div>
